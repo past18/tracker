cd42b0b54b31042cd715f832d3edf77f0c74611f
past18 <pastefinivw@gmail.com>
2022-04-22 15:12:51 +0300

    4. Object state.

---
+++ src/main/java/com/acts/oop/Battery.java
@@ -0,0 +1,18 @@
+package com.acts.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+}
