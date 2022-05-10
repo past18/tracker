143de4c1dfe26325bb8148f383249fa87fe9ce09
past18 <pastefinivw@gmail.com>
2022-05-10 12:55:19 +0300

    14. Tracker .findAll .findByName

--- src/main/java/com/acts/tracker/Tracker.java
+++ src/main/java/com/acts/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package com.acts.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,32 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        int j = 0;
+        Item[] rsl = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            Item i = items[index];
+            if (i != null) {
+                rsl[j] = i;
+                j++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int j = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                rsl[j] = item;
+                j++;
+            }
+        }
+        return Arrays.copyOf(rsl, j);
     }
 }
