10bfe22e71c142eb01ed5da5d0d68a5d97e83c30
past18 <pastefinivw@gmail.com>
2022-05-10 14:15:10 +0300

    15. Tracker .replace

--- src/test/java/com/acts/tracker/TrackerTest.java
+++ src/test/java/com/acts/tracker/TrackerTest.java
@@ -1,4 +1,4 @@
-package  com.acts.tracker;
+package com.acts.tracker;
 
 import org.junit.Test;
 
@@ -15,6 +15,7 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+
     @Test
     public void whenTestFindAll() {
         Tracker tracker = new Tracker();
@@ -25,6 +26,7 @@ public class TrackerTest {
         Item result = tracker.findAll()[0];
         assertThat(result.getName(), is(first.getName()));
     }
+
     @Test
     public void whenTestFindByNameCheckArrayLength() {
         Tracker tracker = new Tracker();
@@ -51,5 +53,18 @@ public class TrackerTest {
         tracker.add(new Item("First"));
         Item[] result = tracker.findByName(second.getName());
         assertThat(result[1].getName(), is(second.getName()));
+    }
+
+    @Test
+    public void whenReplace() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item();
+        bug.setName("Bug");
+        tracker.add(bug);
+        int id = bug.getId();
+        Item bugWithDesc = new Item();
+        bugWithDesc.setName("Bug with description");
+        tracker.replace(id, bugWithDesc);
+        assertThat(tracker.findById(id).getName(), is("Bug with description"));
     }
 }
