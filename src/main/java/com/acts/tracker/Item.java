2ad2ad5d60402e11cff52f20a2f661867187a529
past18 <pastefinivw@gmail.com>
2022-07-08 23:16:50 +0300

    17. Tracker .toString.

--- src/main/java/com/acts/tracker/Item.java
+++ src/main/java/com/acts/tracker/Item.java
@@ -1,11 +1,14 @@
 package com.acts.tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
-    private  LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created = LocalDateTime.now();
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     public Item() {
     }
@@ -38,7 +41,17 @@ public class Item {
     public LocalDateTime getCreated() {
         return created;
     }
-    public void setCreated(LocalDateTime created){
-        this.created=created;
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created.format(FORMATTER) +
+                '}';
     }
 }
