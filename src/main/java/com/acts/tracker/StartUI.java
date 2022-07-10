2f9e00c4d216c77cdb479cf6621612d38e47b20f
past18 <pastefinivw@gmail.com>
2022-07-10 13:49:52 +0300

    19. StartUI tracker .showMenu

--- src/main/java/com/acts/tracker/StartUI.java
+++ src/main/java/com/acts/tracker/StartUI.java
@@ -1,14 +1,40 @@
 package com.acts.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+import java.util.Scanner;
 
 public class StartUI {
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.println("Select:");
+            int select = Integer.parseInt(scanner.nextLine());
+
+            if (select != 6) {
+                System.out.println("User selected: " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+
+    }
+
     public static void main(String[] args) {
-        Item item = new Item();
-        LocalDateTime s = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String ss = s.format(formatter);
-        System.out.println(ss);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
