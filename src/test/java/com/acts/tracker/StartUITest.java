35cf8a10938f4ab40a4d0aaae950983e5fb2b1cb
past18 <pastefinivw@gmail.com>
2022-07-17 19:57:47 +0300

    34. Testing Console output.

--- src/test/java/com/acts/tracker/StartUITest.java
+++ src/test/java/com/acts/tracker/StartUITest.java
@@ -29,9 +29,9 @@ public class StartUITest {
         Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Replaced item"));
-        String replacedName = "New item name" ;
+        String replacedName = "New item name";
         Input in = new StubInput(
-                new String[]{"0", "1", "New item name","1"}
+                new String[]{"0", "1", "New item name", "1"}
         );
         UserActions[] actions = {
                 new EditItem(out),
@@ -56,11 +56,12 @@ public class StartUITest {
         new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
+
     @Test
     public void whenExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"0"}
+                new String[]{"0"}
         );
         Tracker tracker = new Tracker();
         UserActions[] actions = {
@@ -71,6 +72,33 @@ public class StartUITest {
                 "Menu." + System.lineSeparator() +
                         "0. Exit Program" + System.lineSeparator()
         ));
+    }
+
+    @Test
+    public void whenShowAll() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        String replaceName = "New Test Name";
+        Input in = new StubInput(
+                new String[]{"0", "1"}
+        );
+        UserActions[] actions = new UserActions[]{
+                new ShowItems(out),
+
+                new Exit()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(),
+                is(
+                        "Menu.\r\n" +
+                                "0. Show all items\r\n" +
+                                "1. Exit Program\r\n" +
+                                "=== Show all items ===\r\n" +
+                                one + "\r\n" +
+                                "Menu.\r\n" +
+                                "0. Show all items\r\n" +
+                                "1. Exit Program\r\n"));
     }
 
 }
